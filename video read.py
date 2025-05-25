0dac5f4d1b1c2e2b346637e12dbd8ed6a457d9e1
sanjana-ctrl <sanjanabhakta96@gmail.com>
2025-05-25 18:35:21 +0530

    add comment

--- video read.py
+++ video read.py
@@ -1,13 +1,13 @@
 import cv2
-cap=cv2.VideoCapture(0)
-while True:
-    ret,frame=cap.read()
+cap=cv2.VideoCapture(0) #open web cam
+while True: #check if web cam recording if false move out of loop
+    ret,frame=cap.read() 
     if ret==False:
         continue
     cv2.imshow("video frame",frame)
 
-    key_pressed=cv2.waitKey(1)&0xFF
-    if key_pressed==ord('q'):
+    key_pressed=cv2.waitKey(1)&0xFF  #bitwise logic later in data collection
+    if key_pressed==ord('q'): #'q' breaks loop and closes camera 
         break
 cap.release()
 cv2.destroyAllWindows()
