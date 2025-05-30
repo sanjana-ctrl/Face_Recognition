abc1e440ee9d17ebd107381e4130b65502af088b
sanjana-ctrl <sanjanabhakta96@gmail.com>
2025-05-31 04:09:17 +0530

    face_data created

--- face_data.py
+++ face_data.py
@@ -25,3 +25,36 @@ while True:
     k=1
 
     faces=sorted(faces,key=lambda x: x[2]*x[3],reverse=True)
+    skip+=1
+
+    for face in faces[:1]:
+        x,y,w,h=face
+
+        offset=5
+        face_offset=frame[y-offset:y+h+offset,x-offset:x+w+offset]
+        face_selection=cv2.resize(face_offset,(100,100))
+
+        if skip%10==0:
+            face_data.append(face_selection)
+            print(len(face_data))
+        
+        cv2.imshow(str(k),face_selection)
+        k+=1
+
+        cv2.rectangle(frame,(x,y),(x+w,x+h),(0,255,0),2)
+
+    cv2.imshow("faces",frame)
+
+    key_pressed=cv2.waitkey(1) & 0xFF
+    if key_pressed==ord('q'):
+        break
+face_data=np.array(face_data)
+face_data=face_data.reshape((face_data.shape[0],-1))
+print(face_data.shape)
+np.save(dataset_path+file_name,face_data)
+print("Dataset saved at: {}".format(dataset_path+file_name+'.npy'))
+
+cap.release()
+cv2.destroyAllWindows()
+
+
